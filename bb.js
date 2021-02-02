50c0373336fe8b48a813dc2d2fa6598d088b4c10
MR Bappy <bappysarkar2050@gmail.com>
2021-02-02 15:54:32 +0600

    added true false

---
+++ bb.js
@@ -0,0 +1,17 @@
+// Falsy:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy:
+// "0", " ", [], {}
+
+const name = 12;
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
